ec452ffd9b6f7b8b76460243821e6f99d80cb968
ChurongDavidWei <c_wei3@mail.fhsu.edu>
2025-05-04 19:12:14 -0500

    Delete miniproject4 directory

--- miniproject4/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Holding
-
-class HoldingForm(forms.ModelForm):
-    class Meta:
-        model = Holding
-        fields = ['ticker', 'shares', 'purchase_price']
